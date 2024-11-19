88499754e955121f5483c378c7b0ee9d6807be0a
MRHRTZ <hanifsyauqi61@gmail.com>
2024-11-19 23:44:54 +0700

    fix logout message

--- login.php
+++ login.php
@@ -26,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 
 if (isset($_GET['logout'])) {
-    echo "<script>alert('Anda telah logout.')</script>";
+    echo "<script>alert('Anda telah logout.'); window.location.href='login.php'</script>";
 }
 
 ?>
